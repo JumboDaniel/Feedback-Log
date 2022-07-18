aeadcd9dd290f7fb67ce7d94b0eee20555425db0
Jumbo Daniel <jumbodaniel15@gmail.com>
2022-07-18 18:06:26 +0800

    Added the Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+//file imports
 import "../styles/globals.css";
-import{ProvideAuth} from '../lib/auth'
+import { theme } from "@/styles/theme";
+import { ProvideAuth } from "@/lib/auth";
+
+///Node imports
+import { ChakraProvider, CSSReset, GlobalStyle } from "@chakra-ui/react";
+import { jsx } from "@emotion/react";
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider theme={theme}>
+      <ProvideAuth>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ProvideAuth>
+    </ChakraProvider>
   );
 }
 
