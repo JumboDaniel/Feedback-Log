458060822904899e154e315a0ddc56d12e26a8fb
Jumbo Daniel <34845120+JumboDaniel@users.noreply.github.com>
2022-07-18 18:26:23 +0800

    Revert "Feature/dashboard"

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,7 @@
-//file imports
-import "../styles/globals.css";
-import { theme } from "@/styles/theme";
-import { ProvideAuth } from "@/lib/auth";
+import '../styles/globals.css'
 
-///Node imports
-import { ChakraProvider, CSSReset, GlobalStyle } from "@chakra-ui/react";
-import { jsx } from "@emotion/react";
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider theme={theme}>
-      <ProvideAuth>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ProvideAuth>
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
